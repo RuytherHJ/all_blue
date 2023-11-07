61ac000ad366dceaff6b0305ddea883aec98915c
Patrick Ribeiro Peter <20221TPMI0335@cefetes.br>
2023-11-06 21:23:33 -0300

    PHP funciona!!!!!

--- codigos/php/pegar_bd.php
+++ codigos/php/pegar_bd.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost:3306";
 $username = "root";
-$password = "root";
+$password = "";
 $dbname = "all_blue";
 
 // Conectar ao banco de dados
@@ -13,10 +13,9 @@ if ($conn->connect_error) {
 }
 
 // Consulta para obter informações do banco de dados
-$sql = "SELECT pd.nome AS nome_produto, pd.preco AS preco_produto, lj.nome AS nome_loja, lj.url_logo AS logo, mc.nome_fabricante AS marca
-        FROM Tabela_Produtos pd
-        JOIN Tabela_Lojas lj ON pd.id = lj.id
-        JOIN Tabela_Marcas mc ON lj.id = mc.id";
+$sql = "SELECT pd.nome AS nome_produto, pd.preco AS preco_produto, lj.nome AS nome_loja, pd.url_img AS img
+        FROM produtos pd
+        JOIN lojas lj ON pd.id = lj.id;";
 $result = $conn->query($sql);
 
 
@@ -24,16 +23,15 @@ $result = $conn->query($sql);
 
                 
 // Exibir informações do banco de dados
-if ($result->mysqli_num_rows() > 0) {
+if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         echo "Nome do Produto: " . $row["nome_produto"] . "<br>";
-        echo "Preço: " . $row["preco"] . "<br>";
+        echo "Preço: " . $row["preco_produto"] . "<br>";
         echo "Loja: " . $row["nome_loja"] . "<br>";
-        echo "Marca: " . $row["nome_fabricante"] . "<br>";
-        
-        // Exibir a URL da logo da marca
-        echo '<img src="' . $row["logo"] . '" alt="Logo da Marca"><br><br>';
-    }
+        echo '<img src="'. $row["img"] . '">' ;
+
+        }
+
 } else {
     echo "Nenhuma informação encontrada no banco de dados.";
 }
