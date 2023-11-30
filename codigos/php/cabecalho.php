56be5a9767554b0d3a7db89e395a797e282f7e0e
isabellyFaier <isabellyfaierv25@gmail.com>
2023-11-30 11:34:33 -0300

    responsividade do cabeçalho

--- codigos/php/cabecalho.php
+++ codigos/php/cabecalho.php
@@ -16,51 +16,53 @@
 <body>
 
      <!-- Cabeçalho -->
-     <header>
-        <nav>
-            <div class="logo-container">
-                <a href="/all_blue/codigos/php/all_blue.php">
-                <img src="/all_blue/codigos/files/logo_png.png" alt="All Blue" class="logo">
-                </a>
-            </div>
-            <!-- Barra de pesquisa -->
-            <div class="wrap">
-            
-                <form class="search" action="/all_blue/codigos/php/pesquisa.php" method="POST"> 
-
-                    <input type="text" class="searchTerm" name="search_bar" placeholder="O que você procura hoje?">
-                    <button type="submit" class="searchButton">
-                        <i class="fa fa-search"></i>
-                    </button>
-
-                </form>
-    
-            </div>
-
-            <!-- Dropdown menu  -->
-            <div>
-
-                <div class="position">
-                    <!-- Opções do menu -->
-                    <span class="material-symbols-outlined" id="icon"> 
-                        account_circle
-                        </span>
-                    <a href="/all_blue/codigos/php/login.php">Login</a>
-                    <span class="material-symbols-outlined" id= "icon">
-                        person_add
-                        </span>
-                    <a href="/all_blue/codigos/php/cadastro_usuario.php">Cadastro</a>
-                    <span class="material-symbols-outlined" id="icon">
-                        contact_support
-                        </span>
-                    <a href="/all_blue/codigos/php/fale-conosco.php">Fale conosco</a>
+     <div class="cabeçalho">
+        <header>
+            <nav>
+                <div class="logo-container">
+                    <a href="/all_blue/codigos/php/all_blue.php">
+                    <img src="/all_blue/codigos/files/logo_png.png" alt="All Blue" class="logo">
+                    </a>
                 </div>
-            </div>
+                <!-- Barra de pesquisa -->
+                <div class="wrap">
+                
+                    <form class="search" action="/all_blue/codigos/php/pesquisa.php" method="POST"> 
 
-            <!--Dropdown filtro de marca/preço  -->
+                        <input type="text" class="searchTerm" name="search_bar" placeholder="O que você procura hoje?">
+                        <button type="submit" class="searchButton">
+                            <i class="fa fa-search"></i>
+                        </button>
 
-        </nav>
-    </header>
+                    </form>
+        
+                </div>
+
+
+                <div>
+
+                    <div class="position">
+                        <span class="material-symbols-outlined" id="icon"> 
+                            account_circle
+                            </span>
+                        <a href="/all_blue/codigos/php/login.php">Login</a>
+                        <span class="material-symbols-outlined" id= "icon">
+                            person_add
+                            </span>
+                        <a href="/all_blue/codigos/php/cadastro_usuario.php">Cadastro</a>
+                        <span class="material-symbols-outlined" id="icon">
+                            contact_support
+                            </span>
+                        <a href="/all_blue/codigos/php/fale-conosco.php">Fale conosco</a>
+                    </div>
+                </div>
+
+                <!--Dropdown filtro de marca/preço  -->
+
+            </nav>
+        </header>
+     </div>
+     
 
     
 </body>
