27152a27db289eb68d769b109abcc1c45339d32c
Thiago Nunes de Assis <thiagonunesdeassis@gmail.com>
2023-11-30 23:46:45 -0300

    Coloquei os sweetalert

--- codigos/php/cadastro_usuario.php
+++ codigos/php/cadastro_usuario.php
@@ -7,6 +7,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro usuário</title>
+    <link rel="stylesheet" href="/ALL_BLUE/codigos/css/pagina_principal/style_principal.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="/ALL_BLUE/codigos/css/menu/menu_itens.css">
@@ -20,6 +22,66 @@
         <a href="/all_blue/codigos/php/all_blue.php"><img src="/all_blue/codigos/files/logo_png.png" alt="All Blue"></a>
     </div>
         <main>
+
+
+
+
+            <?php
+
+                include 'processar_cadastro.php';
+
+                if (array_key_exists('erro_de_cadastro',$_SESSION) && array_key_exists('senhas_diferentes',$_SESSION) && isset($_SESSION['campo_vazio'])){
+
+                    if($_SESSION['erro_de_cadastro']==true){
+
+                        echo('<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>'); 
+                                echo('<script>Swal.fire({
+                                    icon: "error",
+                                    title: "Esse email já está sendo utilizado!",
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                    });</script>');
+
+                        $_SESSION['erro_de_cadastro']=false;
+                    
+                    
+                    }elseif($_SESSION['senhas_diferentes']==true){
+
+                        echo('<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>'); 
+                        echo('<script>Swal.fire({
+                            icon: "error",
+                            title: "Oops...",
+                            text: "Você inseriu senhas diferentes, por favor, certifique-se o que você digitou!",
+                            
+                        });</script>')   ;
+                        $_SESSION['senhas_diferentes']=false;
+
+                    }
+                    
+                    elseif($_SESSION['campo_vazio']==true){
+                        
+                        echo('<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>'); 
+                                echo('<script>Swal.fire({
+                                    icon: "error",
+                                    title: "Algum dos campos estam vazios!",
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                    });</script>');
+                            
+                        }
+
+                        $_SESSION['campo_vazio']=false;
+
+
+
+                
+                }
+
+
+            ?>
+
+
+        
 
 
             <!-- Formulário Cadastro -->
--- codigos/php/processar_cadastro.php
+++ codigos/php/processar_cadastro.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Configuração de conexão com o banco de dados
     $servername = "localhost:3306";
     $username = "root";
-    $password = "";
+    $password = "thiago";
     $dbname = "all_blue";
 
     // Conecte-se ao banco de dados
@@ -19,34 +19,66 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nome = trim($_POST['user_name']);
     $email = trim($_POST['user_email']);
     $senha = trim($_POST['user_senha']);
+    $confirmadora=trim($_POST['user_senhaconfirma']);
 
     // Inserção dos dados no banco de dados
 
     $sql = "call all_blue.insere_usuario('$email','".sha1($senha)."', '$nome');";
 
 
-    if ($conn->query($sql) === TRUE) {
 
 
-        $_SESSION['nome_logado']=$nome;
-        $_SESSION['usuarioLogado'] = true;
-        
-        $_SESSION['cadastrou']=true;
-        
-        header('Location: /all_blue/codigos/php/all_blue.php');
-        
-        exit();
+    if($senha!=$confirmadora){
 
-    } else {
-        echo "<h1>ERRO AO CADASTRAR!!!</h1>";
-        sleep(2);
+        $_SESSION['campo_vazio']=false;
+        $_SESSION['senhas_diferentes']=true;
+        $_SESSION['erro_de_cadastro']=false;
+
         header('Location: /all_blue/codigos/php/cadastro_usuario.php');
         exit();
+
+
+
+
+
+    }elseif(strlen($senha)>0 && strlen($email)>0 && strlen($nome)>0 && strlen($confirmadora)>0){
+        
+            if ($conn->query($sql) === TRUE) {
+    
+    
+                $_SESSION['nome_logado']=$nome;
+                $_SESSION['usuarioLogado'] = true;
+                
+                $_SESSION['cadastrou']=true;
+                
+                header('Location: /all_blue/codigos/php/all_blue.php');
+                
+                exit();
+        
+            } else {
+                
+                $_SESSION['campo_vazio']=false;
+                $_SESSION['senhas_diferentes']=false;
+                $_SESSION['erro_de_cadastro']=true;
+    
+                header('Location: /all_blue/codigos/php/cadastro_usuario.php');
+                exit();
+            }
+
+    }else{
+        $_SESSION['senhas_diferentes']=false;
+        $_SESSION['erro_de_cadastro']=false;
+        $_SESSION['campo_vazio']=true;
+        header('Location: /all_blue/codigos/php/cadastro_usuario.php');
+        exit();
+
     }
+    
 
     // Feche a conexão com o banco de dados
     $conn->close();
 } else {
-    echo "Método de requisição inválido.";
+
+   
 }
 ?>
