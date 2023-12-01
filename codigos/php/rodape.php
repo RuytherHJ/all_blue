b3d6bdd5fbcb226f209864e6b1dcf652397a79d8
Thiago Nunes de Assis <thiagonunesdeassis@gmail.com>
2023-12-01 14:50:23 -0300

    Criei esse rodapé no desespero

---
+++ codigos/php/rodape.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/ALL_BLUE/codigos/css/pagina_principal/style_principal.css">
+    <title>Rodapé</title>
+</head>
+<body>
+    <footer> Copyright © 2023 </footer>
+    
+</body>
+</html>
