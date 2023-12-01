d0dbfe4013933943396231e97281833181d6b302
Patrick Ribeiro Peter <20221TPMI0335@cefetes.br>
2023-12-01 17:25:48 -0300

    opdates

--- Scraping/KABUM.py
+++ Scraping/KABUM.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 import mysql.connector                 #   pip install mysql-connector-python               
 from mysql.connector import errorcode
 
-banco=mysql.connector.connect(host='localhost', database='all_blue', user='root',password='thiago')
+banco=mysql.connector.connect(host='localhost', database='all_blue', user='root',password='')
 
 cursor=banco.cursor(buffered=True)
 
---
+++ codigos/php/rodape.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/ALL_BLUE/codigos/css/pagina_principal/style_principal.css">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Rodapé -->
+    <footer style="text-align: center; padding-top: 20px; padding-bottom: 20px; background-image: linear-gradient( to left, #431bb1, rgb(21, 125, 221) ); 
+; width: 100%; color: white">
+    <p>&copy; <?php echo date("Y"); ?> All Blue</p>
+</footer>
+</body>
+</html>
